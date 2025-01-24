ab2a5ea6d305be30e1e94ed55b091adcd8de724e
ritik <rajputritik8660@gmail.com>
2025-01-24 12:07:37 +0530

    Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Header from './components/Header'
 function App() {
   return (
     <>
-     <h1>Hello</h1>
+     <BrowserRouter>
+     <Header/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signout' element={<Signout/>}/>
+     </Routes>
+     
+
+
+
+
+     </BrowserRouter>
     </>
   )
 }
